c1c394680fb4377f7fd3f8d88c3ed9704b1c7eb2
Eva Zmazek <eva.zmazek@gmail.com>
2017-10-28 20:39:05 +0200

    brute force metoda
    delujoča brute force metoda

--- brute_force_SAT.py
+++ brute_force_SAT.py
@@ -1,22 +1,41 @@
 from boolean import *
 
-def get_all_variables(spremenljivke,formula):
-    if not isinstance(formula, Formula):
-        return Error
-    elif isinstance(formula, Variable):
-        spremenljivke = spremenljivke + [formula.x]
+def get_all(formula, spremenljivke=set()):
+    if isinstance(formula, Variable):
+        spremenljivke = spremenljivke.union(formula.x)
     elif isinstance(formula, Not):
-        spremenljivke = spremenljivke + [formula.x]
-    elif isinstance(formula, Or):
-        for clen in formula.terms:
-            get_all_variables(spremenljivke, clen)
-    elif isinstance(formula, And):
-        for clen in formula.terms:
-            get_all_variables(spremenljivke, clen)
+        spremenljivke = spremenljivke.union(get_all(formula.x))
+    elif isinstance(formula, And) or isinstance(formula, Or):
+        for term in formula.terms:
+            spremenljivke = spremenljivke.union(get_all(term))
     else:
-        return "Kaj točno pa še ostane?"
+        print("ni še obdelan if")
+    return spremenljivke
 
-def brut_force(spremenljivke):
-    if len(spremenljivke)==0:
-        print("nekaj")
-        
+def list_spremenljivk(formula):
+    spremenljivke = list(get_all(formula))
+    return spremenljivke
+
+def brute_force(formula):
+    spremenljivke = list_spremenljivk(formula)
+    def search(spremenljivke, v, formula):
+        if len(spremenljivke)==0:
+            if formula.evaluate(v):
+                print(v)
+                return True
+            else:
+                return False
+        else:
+            spremenljivka = spremenljivke[0]
+            spremenljivke = spremenljivke[1:]
+            v[spremenljivka] = True
+            if search(spremenljivke, v, formula):
+                return True
+            v[spremenljivka] = False
+            if search(spremenljivke, v, formula):
+                return True
+            return False
+    if search(spremenljivke, dict(), formula):
+        return "formula je satisfiable"
+    else:
+        return "formula ni satisfiable"
